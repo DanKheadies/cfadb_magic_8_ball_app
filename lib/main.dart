21f32c1dbda560c6b4fe22ec61403723fbc62b67
Papa Daco <papadaco@PapaDaco.local>
2021-05-31 00:23:02 -0400

    Ask Zultar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(Magic8BallApp());
+}
+
+class Magic8BallApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ask Zultar'),
+          backgroundColor: Colors.indigo,
+        ),
+        body: Magic8BallPage(),
+        backgroundColor: Colors.indigoAccent.shade100,
       ),
     );
+  }
+}
+
+class Magic8BallPage extends StatefulWidget {
+  @override
+  _Magic8BallPageState createState() => _Magic8BallPageState();
+}
+
+class _Magic8BallPageState extends State<Magic8BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: TextButton(
+          onPressed: () {
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
